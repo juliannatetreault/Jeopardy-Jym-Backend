dbb96ca31a48761ea21eda44df61d0a0e081ecda
github username <github email address>
2019-02-26 17:38:38 -0700

    added further seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Flashcard.create([
+{question: 'The ingredients of this basic salad dressing might be the olive & balsamic types, respectively.', answer: 'Oil & Vinegar', category: '____&____' }, 
+{question: '"The Federalist" No. 51 argues for the Constitution\'s "Partitian of Power", now called this system.', answer: 'Checks & Balances', category: '____&____'},
+{question: '"Let us go in, the fog is rising" were said to be the last words of this poet.', answer: 'Dickinson', category: '"Dic"-tionary'},
+{question: '6-letter term for a judicial assertion or an edict.', answer: 'Dictum', category: '"Dic"-tionary'},
+{question: 'A sign that points to the inability to sleep.', answer: 'Insomnia Indicator', category: 'A Little Alliteration'},
+{question: 'The king of beasts\' maze.', answer: 'Lion\'s Labyrinth', category: 'A Little Alliteration'}
+])
