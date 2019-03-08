e1e6c964c21903739603799e9b2e428cdf0771ef
github username <github email address>
2019-03-07 21:24:20 -0700

    added more seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,5 +13,11 @@ Flashcard.create([
 {question: '"Let us go in, the fog is rising" were said to be the last words of this poet.', answer: 'Dickinson', category: '"Dic"-tionary'},
 {question: '6-letter term for a judicial assertion or an edict.', answer: 'Dictum', category: '"Dic"-tionary'},
 {question: 'A sign that points to the inability to sleep.', answer: 'Insomnia Indicator', category: 'A Little Alliteration'},
-{question: 'The king of beasts\' maze.', answer: 'Lion\'s Labyrinth', category: 'A Little Alliteration'}
+{question: 'The king of beasts\' maze.', answer: 'Lion\'s Labyrinth', category: 'A Little Alliteration'},
+{question: 'Traditionally served in a copper mug, this cocktail is vodka, lemon juice,& ginger beer; it\'s got a real kick.', answer: 'Moscow Mule', category: 'Alliterative Food & Drink'},
+{question: 'The coctail of gin, cherry brandy, & lemon juice are named for a city near the Malay Peninsula.', answer: 'Singapore Sling', category: 'Alliterative Food & Drink'},
+{question: '"Just sit right back and you\'ll hear a tale."', answer: 'Gilligan\'s Island', category: 'TV Theme Songs'},
+{question: '"I\'ll be there for you when the rain starts to pour, I\'ll be there for you like I\'ve been there before."', answer: 'Friends', category: 'TV Theme Songs'},
+{question: 'Originally, the title woman was called Miss Daisy Hawkins, but that didn\'t sound lonely enough.', answer: 'Eleanor Rigby', category: 'Beatles Songs'},
+{question: 'He\'s "As blind as he can be, just sees what he wants to see."', answer: 'Nowhere Man', category: 'Beatles Songs'}
 ])
